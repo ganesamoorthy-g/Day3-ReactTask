7cf3cfdd27655f83c35409e35e64bca4f3540b5f
ganesamoorthy-g <gurunathganesh6@gmail.com>
2023-06-28 23:38:58 +0530

    Initial commit

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,47 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function Card({ setCartValue }) {
-  let [toggle, setToggle] = useState(true)
-  return <>
+  const [toggle, setToggle] = useState(true);
+
+  const handleAddToCart = () => {
+    setCartValue(value => value + 1);
+    setToggle(false);
+  };
+
+  const handleRemove = () => {
+    setCartValue(value => value - 1);
+    setToggle(true);
+  };
+
+  return (
     <div className="col mb-5">
       <div className="card h-100">
-        <div className="badge bg-dark text-white position-absolute" style={{ top: '0.5rem', right: '0.5rem' }}>Sale</div>
-            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        {toggle && (
+          <div className="badge bg-dark text-white position-absolute" style={{ top: '0.5rem', right: '0.5rem' }}>
+            Sale
+          </div>
+        )}
+        <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
         <div className="card-body p-4">
           <div className="text-center">
             <div className="d-flex justify-content-center small text-warning mb-2">
-
               <div className="bi-star-fill"></div>
               <div className="bi-star-fill"></div>
               <div className="bi-star-fill"></div>
               <div className="bi-star-fill"></div>
               <div className="bi-star-fill"></div>
             </div>
-
             <h5 className="fw-bolder">Fancy Product</h5>
             $40.00 - $80.00
           </div>
-
         </div>
         <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
           <div className="text-center">
-            {
-              toggle ? <button className="btn btn-outline-dark mt-auto" onClick={() => {
-                setCartValue(value => value + 1)
-                setToggle(false)
-              }}>
+            {toggle ? (
+              <button className="btn btn-outline-dark mt-auto" onClick={handleAddToCart}>
                 Add To Cart
-              </button> : <button className="btn btn-outline-dark mt-auto" onClick={() => {
-                setCartValue(value => value - 1)
-                setToggle(true)
-              }}> Remove
               </button>
-            }
+            ) : (
+              <button className="btn btn-outline-dark mt-auto" onClick={handleRemove}>
+                Remove
+              </button>
+            )}
           </div>
         </div>
       </div>
     </div>
-  </>
+  );
 }
 
-export default Card
+export default Card;
